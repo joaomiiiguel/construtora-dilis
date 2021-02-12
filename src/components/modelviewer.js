e7857c0c88def2450804830564ef90882155a2b2
joaomiiiguel <joaomiguel.farias@gmail.com>
2021-02-12 12:33:19 -0300

    adasd

--- src/components/modelviewer.js
+++ src/components/modelviewer.js
@@ -2,7 +2,7 @@ import React from 'react';
 import '@google/model-viewer';
 import '../App.css'
 import { Button } from '@material-ui/core';
-import {Helmet} from "react-helmet";
+import { Helmet } from "react-helmet";
 import ModelGlt from '../assets/building.glb'
 import ModelUsdz from '../assets/building.usdz'
 import btnAR from '../assets/arIcon.svg'
@@ -13,6 +13,22 @@ export default function modelviewer() {
             <Helmet>
                 <script type="module" src="https://unpkg.com/@google/model-viewer/dist/model-viewer.min.js"></script>
             </Helmet>
+
+            <model-viewer src={ModelGlt} camera-controls autoplay animation-name="ORBITA" ar ar-modes="scene-viewer webxr quick-look" ios-src={ModelUsdz}>
+
+                <div class="progress-bar hide" slot="progress-bar">
+                    <div class="update-bar"></div>
+                </div>
+                <Button slot="ar-button" id="ar-button">
+                    <img src={btnAR} className="btnAR"/>
+                </Button>
+            </model-viewer>
+
+
+        </div>
+    )
+}
+            /**
             <model-viewer
                 src={ModelGlt}
                 ios-src={ModelUsdz}
@@ -26,8 +42,4 @@ export default function modelviewer() {
                 <Button slot="ar-button" id="ar-button">
                     <img src={btnAR} className="btnAR"/>
                 </Button>
-            </model-viewer>
-
-        </div>
-    )
-}
+            </model-viewer> */
