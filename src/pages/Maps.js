c2370b0d0b0b473c1ed31f43e012fed4e0d3b643
joaomiiiguel <joaomiguel.farias@gmail.com>
2021-02-17 10:50:27 -0300

    Update Maps.js

--- src/pages/Maps.js
+++ src/pages/Maps.js
@@ -1,20 +1,20 @@
 import React from 'react'
-import { GoogleMap, Marker } from "react-google-maps"
+//import { GoogleMap, Marker } from "react-google-maps"
 
-const MyMapComponent = (props) =>
+/*const MyMapComponent = (props) =>
   <GoogleMap
     defaultZoom={8}
     defaultCenter={{ lat: -34.397, lng: 150.644 }}
   >
     {props.isMarkerShown && <Marker position={{ lat: -34.397, lng: 150.644 }} />}
   </GoogleMap>
-
+*/
 export default function Maps(props) {
     return (
         <div>
             <h1>Mapa</h1>
 
-            <MyMapComponent isMarkerShown />
+            
         </div>
     )
 }
