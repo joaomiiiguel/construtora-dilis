2dca97d563758ec8cb0d30714cf1904dd594f239
joaomiiiguel <joaomiguel.farias@gmail.com>
2021-02-23 16:37:09 -0300

    Atualizacao 23-02

--- src/pages/Maps.js
+++ src/pages/Maps.js
@@ -1,19 +1,21 @@
 import React from 'react'
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
-import "leaflet/dist/leaflet.css";
+
+
 
 export default function Maps(props) {
   const position = [-7.2232, -35.8907]
+
   return (
     <div>
-      <MapContainer center={position} zoom={14}>
+      <MapContainer center={position} zoom={15}>
         <TileLayer
           attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         />
-        <Marker position={position}>
+        <Marker position={position} >
           <Popup>
-            A pretty CSS3 popup. <br /> Easily customizable.
+            Novo escritório <br /> Dilis GS
           </Popup>
         </Marker>
       </MapContainer>
